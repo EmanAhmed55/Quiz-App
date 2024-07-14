4a09d283383f50820cd8b2ebb839a45ca90bb1f5
EmanAhmed55 <emanahmed36958@gmail.com>
2024-07-14 23:16:22 +0300

    Simple Quiz App using HTML5 , CSS and JavaScript

---
+++ Quiz-App/questions.js
@@ -0,0 +1,48 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following is not a valid phase of Software Development Life Cycle (SDLC)?",
+    answer: "Testing closure",
+    options: [
+      "Testing phase",
+      "Requirement phase",
+      "Deployment phase",
+      "Testing closure"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following testing is also known as white-box testing?",
+    answer: "Structural testing",
+    options: [
+      "Error guessing technique",
+      "Structural testing",
+      "Design based testing",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following testing is related to the boundary value analysis?",
+    answer: "Black-box testing",
+    options: [
+      "White-box testing",
+      "Black-box testing",
+      "Black-box testing & white-box testing",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What are the different levels of Testing?",
+    answer: "All of the above",
+    options: [
+      "Unit testing",
+      "Integration testing",
+      "System testing",
+      "All of the above"
+    ]
+  }
+    
+];
